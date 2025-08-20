8f60fec1a5900a474beef9b6e6d9906996f0c19d
vsvt2000 <viswajithvishnusai@gmail.com>
2025-08-21 01:09:16 +0530

    blog updates and changes

--- src/pages/blog/[name]/index.tsx
+++ src/pages/blog/[name]/index.tsx
@@ -1,18 +1,24 @@
 import { htmlBlogInfo } from "@/utils/blogs";
 import React from "react";
 import { useRouter } from "next/router";
+import Header from "@/components/header";
+import { geistMono, geistSans } from "@/utils/fonts";
 
 function BlogHomePage() {
   const router = useRouter();
 
-  const name = router?.query?.name as unknown as string || "market_segmentation";
+  const name =
+    (router?.query?.name as unknown as string) || "market_segmentation";
   const fileName = htmlBlogInfo[name];
   return (
-    <div className="w-full h-full overflow-hidden">
-      <iframe
-        src={`/html_content/${fileName}.html`}
-        className="w-full h-screen border-0"
-      />
+    <div className={`${geistSans.className} ${geistMono.className}`}>
+      <Header />
+      <div className="w-full h-full overflow-hidden">
+        <iframe
+          src={`/html_content/${fileName}.html`}
+          className="w-full h-screen border-0"
+        />
+      </div>
     </div>
   );
 }
